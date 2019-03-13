e3f06664aa771be6ef9b51985bae77a040e94715
Marcin Szuppe <marcin.szuppe@clurgo.com>
2019-03-13 10:01:47 +0100

    Dockerfile for nginx

--- quotes/src/app/app.component.ts
+++ quotes/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'quotes';
+@Injectable()
+export class AppComponent implements OnInit {
+  title = 'Quotes Application';
+
+  quote: string;
+  author: string;
+  quotesUrl = 'http://quotes-svc:38070/quote';
+
+  constructor(private http: HttpClient) {
+    this.quote = "Ala ma kota";
+    this.author = "Ala";
+  }
+
+  ngOnInit() {
+    // this.randomize();
+  }
+
+  randomize() {
+    this.getRandomQuote()
+      .subscribe((data) => {
+        this.quote = data['content'];
+        this.author = data['author'];
+      });
+  }
+
+  getRandomQuote() {
+    return this.http.get(this.quotesUrl);
+  }
 }
--- quotes/src/app/app.module.ts
+++ quotes/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { RandomComponent } from './random/random.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RandomComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
